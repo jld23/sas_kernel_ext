a855c04722bba550a2c2ef927fead8cf4a056edc
Jared Dean <jared.dean@sas.com>
2020-12-08 16:43:13 -0500

    inital push for file menu create SAS program

---
+++ jl-create-sas-file/README.md
@@ -0,0 +1,59 @@
+# jl-create-sas-file
+
+![Github Actions Status](https://github.com/jld23/sas_kernel_ext/workflows/Build/badge.svg)
+
+A small extension to add create SAS file to tool bar
+
+
+
+## Requirements
+
+* JupyterLab >= 2.0
+
+## Install
+
+```bash
+jupyter labextension install jl-create-sas-file
+```
+
+## Contributing
+
+### Install
+
+The `jlpm` command is JupyterLab's pinned version of
+[yarn](https://yarnpkg.com/) that is installed with JupyterLab. You may use
+`yarn` or `npm` in lieu of `jlpm` below.
+
+```bash
+# Clone the repo to your local environment
+# Move to jl-create-sas-file directory
+
+# Install dependencies
+jlpm
+# Build Typescript source
+jlpm build
+# Link your development version of the extension with JupyterLab
+jupyter labextension install .
+# Rebuild Typescript source after making changes
+jlpm build
+# Rebuild JupyterLab after making any changes
+jupyter lab build
+```
+
+You can watch the source directory and run JupyterLab in watch mode to watch for changes in the extension's source and automatically rebuild the extension and application.
+
+```bash
+# Watch the source directory in another terminal tab
+jlpm watch
+# Run jupyterlab in watch mode in one terminal tab
+jupyter lab --watch
+```
+
+Now every change will be built locally and bundled into JupyterLab. Be sure to refresh your browser page after saving file changes to reload the extension (note: you'll need to wait for webpack to finish, which can take 10s+ at times).
+
+### Uninstall
+
+```bash
+
+jupyter labextension uninstall jl-create-sas-file
+```
---
+++ jl-create-sas-file/src/index.ts
@@ -0,0 +1,94 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { ICommandPalette } from '@jupyterlab/apputils';
+
+import { ILauncher } from '@jupyterlab/launcher';
+
+import { IFileBrowserFactory } from '@jupyterlab/filebrowser';
+
+import { IMainMenu } from '@jupyterlab/mainmenu';
+
+import '../style/index.css';
+
+//import {sasIcon}  from './CodeSnippetLangues';
+
+
+
+const FACTORY = 'Editor';
+const PALETTE_CATEGORY = 'Text Editor';
+//const ICON_CLASS = sasIcon;
+
+
+namespace CommandIDs {
+  export const createNew = 'fileeditor:create-new-sas-file';
+}
+
+
+
+/**
+ * Initialization data for the jl-create-sas-file extension.
+ */
+const extension: JupyterFrontEndPlugin<void> = {
+  id: 'jl-create-sas-file',
+  autoStart: true,
+  requires: [IFileBrowserFactory],
+  optional: [ILauncher, IMainMenu, ICommandPalette],
+  activate: (
+    app: JupyterFrontEnd,
+    browserFactory: IFileBrowserFactory,
+    launcher: ILauncher,
+    menu: IMainMenu | null,
+    palette: ICommandPalette
+  ) => {
+    const { commands } = app;
+
+    const command = CommandIDs.createNew;
+
+    commands.addCommand(command, {
+      label: args => (args['isPalette'] ? 'New SAS File' : 'SAS File'),
+      caption: 'Create a new SAS file',
+      //TODO: Show the SAS Icon next to the entry.
+      //iconClass: args => (args['isPalette'] ? '' : ICON_CLASS),
+      execute: async args => {
+        const cwd = args['cwd'] || browserFactory.defaultBrowser.model.path;
+        const model = await commands.execute('docmanager:new-untitled', {
+          path: cwd,
+          type: 'file',
+          ext: 'sas'
+        });
+        return commands.execute('docmanager:open', {
+          path: model.path,
+          factory: FACTORY
+        });
+      }
+    });
+
+    // add to the launcher
+    if (launcher) {
+      launcher.add({
+        command,
+        category: 'Other',
+        rank: 1
+      });
+    }
+
+    // add to the palette
+    if (palette) {
+      palette.addItem({
+        command,
+        args: { isPalette: true },
+        category: PALETTE_CATEGORY
+      });
+    }
+
+    // add to the menu
+    if (menu) {
+      menu.fileMenu.newMenu.addGroup([{ command }], 30);
+    }
+  }
+};
+
+export default extension;
