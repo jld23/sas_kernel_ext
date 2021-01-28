7af83d2d1cba20d27128b74755a2e9cc4dbdcf1d
Jared Dean <jared.dean@sas.com>
2021-01-28 01:31:59 -0500

    delete old version s with bad names

--- jl-sas-to-nb/src/CodeSnippetLangues.ts
+++
@@ -1,8 +0,0 @@
-import { LabIcon } from '@jupyterlab/ui-components';
-
-import sasSVGstr from '../style/icon/language_icons/sas.svg';
-
-export const sasIcon = new LabIcon({
-    name: 'custom-ui-components:sas',
-    svgstr: sasSVGstr
-  });
