f9a6873d1ac5491b459fb22d12cd657cbb4a64b1
Jared Dean <jared.dean@sas.com>
2021-02-15 11:03:24 -0500

    remove old broken extensions

--- jlab_saslog_view/src/iconImport.ts
+++
@@ -1,8 +0,0 @@
-import { LabIcon } from '@jupyterlab/ui-components';
-
-import sasLogSVGstr from '../style/icon/Jupyter_Log.svg';
-
-export const sasLogIcon = new LabIcon({
-    name: 'custom-ui-components:sas',
-    svgstr: sasLogSVGstr
-  });
--- sas_log_viewer_v1/src/iconImport.ts
+++
@@ -1,8 +0,0 @@
-import { LabIcon } from '@jupyterlab/ui-components';
-
-import sasLogSVGstr from '../style/icons/Jupyter_Log.svg';
-
-export const sasLogIcon = new LabIcon({
-    name: 'custom-ui-components:sasLog',
-    svgstr: sasLogSVGstr
-  });
