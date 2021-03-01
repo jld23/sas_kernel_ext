54d7b83976be86265af7ac2cedca8c1e0ca8ede5
Jared Dean <jared.dean@sas.com>
2021-03-01 16:11:52 -0500

    add input list for saspy cfg

--- sas7bdatProfile/src/index.ts
+++ sas7bdatProfile/src/index.ts
@@ -5,7 +5,7 @@ import {
 
 import { IFileBrowserFactory } from '@jupyterlab/filebrowser';
 import { buildIcon } from '@jupyterlab/ui-components';
-import { showDialog, Dialog } from '@jupyterlab/apputils';
+import { showDialog, Dialog, InputDialog } from '@jupyterlab/apputils';
 import { requestAPI } from './handler';
 
 /**
@@ -17,6 +17,10 @@ const extension: JupyterFrontEndPlugin<void> = {
   requires: [IFileBrowserFactory],
   activate: async function (app: JupyterFrontEnd, factory: IFileBrowserFactory) {
     console.log('JupyterLab extension sas7bdatProfile is activated!');
+    let saspy_cfg = new String();
+    let current_dir = new String();
+    let local_config = new Boolean();
+    // let config_data = new Object();
     app.docRegistry.addFileType({
       name: 'sas7bdat',
       // icon: sasIcon,
@@ -31,8 +35,18 @@ const extension: JupyterFrontEndPlugin<void> = {
     await requestAPI<any>('profile')
       .then(data => {
         console.log(data);
-        // list of configurations current registered
-        //  TODO: data needs to be scoped to be passable to POST request
+        local_config = data.local_config;
+        current_dir = data.current_dir;
+        // Request a choice from a list
+        InputDialog.getItem({
+          title: 'Select SASPy Configuration:',
+          label: 'Choose your configuration:',
+          items: data.saspy_configs
+        }).then(value => {
+          saspy_cfg = value.value;
+          console.log('SASPy Configuration: ' + saspy_cfg);
+        });
+
         console.log(data['saspy_configs']);
       })
       .catch(reason => {
@@ -50,11 +64,9 @@ const extension: JupyterFrontEndPlugin<void> = {
         const dataToSend = {
           fpath: file.path,
           origin: origin,
-          //  TODO: once data is global these won't need to be hard coded
-          cfgname: 'localhost',
-          // local_config: data_from_get['local_config']
-          current_dir: '/home/dsw/source/github/sas_kernel_ext',
-          local_config: true
+          cfgname: saspy_cfg,
+          local_config: local_config,
+          current_dir: current_dir,
 
         };
         await requestAPI<any>('profile', {
@@ -68,27 +80,12 @@ const extension: JupyterFrontEndPlugin<void> = {
             hasClose: false,
             buttons: [Dialog.okButton()]
           })
+
             .catch(reason => {
               console.error(`Error on POST /sas7dbdatProfile/ ${dataToSend}.\n${reason}`);
             });
 
         });
-        // try {
-        //   const reply = await requestAPI<any>('profile', {
-        //     body: JSON.stringify(dataToSend),
-        //     method: 'POST',
-        //   });
-        //   console.log(reply);
-        //   showDialog({
-        //     title: 'Profiled',
-        //     hasClose: false,
-        //     buttons: [Dialog.okButton()]
-        //   })
-        // } catch (reason) {
-        //   console.error(
-        //     `Error on POST /sas7dbdatProfile/ ${dataToSend}.\n${reason}`
-        //   );
-        // }
       }
     });
     app.contextMenu.addItem({
