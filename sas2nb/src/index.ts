22517ad9cd088f7a9fc7a864515302be6038732a
Jared Dean <jared.dean@sas.com>
2021-03-18 11:43:26 -0400

    minor fix no effect.

--- sas2nb/src/index.ts
+++ sas2nb/src/index.ts
@@ -36,41 +36,39 @@ const extension: JupyterFrontEndPlugin<void> = {
         console.log(data);
       })
       .catch(reason => {
-        console.error(
-          `The sas2nb server extension appears to be missing.\n${reason}`
-        );
+        console.error(`The sas2nb server extension appears to be missing.\n${reason}`);
       });
-      app.commands.addCommand('sas2nb/context-menu:open', {
-        label: 'Convert SAS to ipynb',
-        caption: 'Convert a SAS file to a notebook',
-        icon: sasIcon,
-        execute: async () => {
-          const file = factory.tracker.currentWidget.selectedItems().next();
-  
-          // POST request
-          const dataToSend = { fpath: file.path, origin: origin };
-          try {
-            const reply = await requestAPI<any>('convert', {
-              body: JSON.stringify(dataToSend),
-              method: 'POST',
-            });
-            console.log(reply);
-            showDialog({
-              title: 'Converted',
-              body: `${file.path} was converted to a notebook.`,
-              buttons: [Dialog.okButton()],
-            }).catch((e) => console.log(e));
-          } catch (reason) {
-            console.error(`Error on POST /sas2nb/convert ${dataToSend}.\n${reason}`);
-          }
-        },
-      });
-  
-      app.contextMenu.addItem({
-        command: 'sas2nb/context-menu:open',
-        selector: '.jp-DirListing-item[data-file-type="sas"]',
-        rank: 0,
-      });
+    app.commands.addCommand('sas2nb/context-menu:open', {
+      label: 'Convert SAS to ipynb',
+      caption: 'Convert a SAS file to a notebook',
+      icon: sasIcon,
+      execute: async () => {
+        const file = factory.tracker.currentWidget.selectedItems().next();
+
+        // POST request
+        const dataToSend = { fpath: file.path, origin: origin };
+        try {
+          const reply = await requestAPI<any>('convert', {
+            body: JSON.stringify(dataToSend),
+            method: 'POST',
+          });
+          console.log(reply);
+          showDialog({
+            title: 'Converted',
+            body: `${file.path} was converted to a notebook.`,
+            buttons: [Dialog.okButton()],
+          }).catch((e) => console.log(e));
+        } catch (reason) {
+          console.error(`Error on POST /sas2nb/convert ${dataToSend}.\n${reason}`);
+        }
+      },
+    });
+
+    app.contextMenu.addItem({
+      command: 'sas2nb/context-menu:open',
+      selector: '.jp-DirListing-item[data-file-type="sas"]',
+      rank: 0,
+    });
   }
 };
 
