4b00d255e44fbe0ac0a0fc549498089c6136bcbb
bozidar zecevic <bozidar.zecevic@sas.com>
2021-07-19 16:15:35 +0200

    remove commented out code from sascfg_personal.py

--- binder/sascfg_personal.py
+++ binder/sascfg_personal.py
@@ -8,13 +8,3 @@ viya4 = {
     'client_secret' : "7c301751-3114-4554-ad82-e20e3466bb17",
 
     }
-# SAS_config_names = ['viya4']
-#
-# viya4 = {
-#     'url':'https://v4.boemskats.com',
-#     'verify': False,
-#     'context':'SAS Job Execution compute context',
-#     'client_id'     : "SASpy",
-#     'client_secret' : "2aa3c1d5-b63b-404d-b540-7c16dbffbb37",
-#
-#     }
