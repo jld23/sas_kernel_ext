65767576e9105125bc95746a4c4fb686225322be
Jared Dean <jared.dean@sas.com>
2021-07-19 15:34:02 -0400

    add supporting files.

---
+++ sas_log_viewer/src/iconImport.ts
@@ -0,0 +1,7 @@
+import { LabIcon } from '@jupyterlab/ui-components';
+import sasLogSVGstr from '../style/icons/Jupyter_Log.svg';
+
+export const sasLogIcon = new LabIcon({
+    name: 'custom-ui-components:sasLog',
+    svgstr: sasLogSVGstr
+  });
